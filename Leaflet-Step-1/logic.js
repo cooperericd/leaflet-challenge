a9150a0e9ae3a33f867c8c8881458fb9f682b5f9
EricDC <cooper.ericd@gmail.com>
2019-11-23 12:15:13 -0500

    Added legend to map and changed tile layer

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -7,7 +7,7 @@ var myMap = L.map("map", {
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets-basic",
+    id: "mapbox.light",
     accessToken: API_KEY
   }).addTo(myMap);
   
@@ -43,26 +43,25 @@ d3.json(url, function(data) {
       
   }
 
+  //Add legend
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (leg) {
+
+	var div = L.DomUtil.create('div', 'info legend');
+		grades = [0, 1, 2, 3, 4, 5];
+		labels = [];
+
+	for (var i = 0; i < grades.length; i++) {
+		div.innerHTML +=
+			'<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+			grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	}
+
+	return div;
+};
+
+legend.addTo(myMap);
+
 });
-
-//Add legend
-// var legend = L.control({position: 'bottomright'});
-
-// legend.onAdd = function (map) {
-
-// 	var div = L.DomUtil.create('div', 'info legend'),
-// 		grades = [0, 1, 2, 3, 4, 5],
-// 		labels = [];
-
-// 	for (var i = 0; i < grades.length; i++) {
-// 		div.innerHTML +=
-// 			'<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-// 			grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-// 	}
-
-// 	return div;
-// };
-
-// legend.addTo(myMap);
-
 
